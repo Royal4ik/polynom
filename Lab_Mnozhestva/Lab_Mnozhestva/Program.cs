950a7558565fe63d2ad2aab4c825bde66e40d22b
Royal4ik <art.sandin@yandex.ru>
2015-09-13 18:31:33 +0300

    first version

---
+++ Lab_Mnozhestva/Lab_Mnozhestva/Program.cs
@@ -0,0 +1,172 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   Defines the Worker type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Polinoms
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
+    using Polinom_Library;
+
+    [SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1202:ElementsMustBeOrderedByAccess", Justification = "Reviewed. Suppression is OK here."),SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1202:ElementsMustBeOrderedByAccess", Justification = "Reviewed. Suppression is OK here."),SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Reviewed. Suppression is OK here.")]
+    public class Program
+    {
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Reviewed. Suppression is OK here.")]
+        public static Polynom InputPolynom()
+        {
+            Console.WriteLine("Введите cтепень полинома");
+            var deg = Convert.ToInt16(Console.ReadLine());
+            var array1 = new double[deg + 1];
+            for (var i = 0; i <= deg; i++)
+            {
+                Console.Write("Введите коэффицент при {0}-й степени ", i);
+                var koef = Convert.ToInt16(Console.ReadLine());
+                array1[i] = koef;
+            }
+
+            return new Polynom(deg, array1);
+        }
+
+        public static void Main(string[] args)
+        {
+            var compare = -1;
+            while (compare != 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Выберите из списка операцию с полиномом:");
+                Console.WriteLine("1. Вычисление массива значений от массива аргументов");
+                Console.WriteLine("2. Cложение полиномов");
+                Console.WriteLine("3. Вычитание полиномов");
+                Console.WriteLine("4. Умножение полинома на число");
+                Console.WriteLine("5. Умножение полиномов");
+                Console.WriteLine("6. Bычисление корня численным способом на заданном отрезке");
+                Console.WriteLine("0. Exit");
+                
+                compare = Convert.ToInt16(Console.ReadLine());
+                switch (compare)
+                {
+                    case 0:
+                        {
+                            break;
+                        }
+
+                    case 1:
+                        {
+                            Console.WriteLine("Введите полином:");
+                            var polynom = InputPolynom();
+                            Console.WriteLine("Введите количество элемeнтов массива значений");
+                            var number = Convert.ToInt16(Console.ReadLine());
+                            Console.WriteLine("Введите значения элементов");
+                            var arguments = new double[number];
+                            for (var i = 0; i < number; i++)
+                            {
+                                arguments[i] = Convert.ToDouble(Console.ReadLine());
+                            }
+
+                            Console.WriteLine();
+                            Console.WriteLine("Получившиеся значения");
+                            var values = polynom.Calculate(arguments);
+                            for (var i = 0; i < number; i++)
+                            {
+                                Console.Write(values[i] + " ");
+                            }
+
+                            break;
+                        }
+
+                    case 2:
+                        {
+                            Console.WriteLine("Введите первый полином:");
+                            var polynom1 = InputPolynom();
+                            Console.WriteLine("Введите второй полином:");
+                            var polynom2 = InputPolynom();
+                            Console.WriteLine("Сумма введенных полином равна:");
+                            var result = polynom1 + polynom2;
+                            result.PrintPol();
+                            break;
+                        }
+
+                    case 3:
+                        {
+                            Console.WriteLine("Введите первый полином(уменьшаемое):");
+                            var polynom1 = InputPolynom();
+                            Console.WriteLine("Введите второй полином(вычитаемое):");
+                            var polynom2 = InputPolynom();
+                            Console.WriteLine("Разность введенных полином равна:");
+                            var result = polynom1 - polynom2;
+                            result.PrintPol();
+                            break;               
+                        }
+
+                    case 4:
+                        {
+                            Console.WriteLine("Введите полином:");
+                            var polynom = InputPolynom();
+                            Console.WriteLine("Введите число на которое хотите его умножить:");
+                            var multiplier = Convert.ToDouble(Console.ReadLine());
+                            Console.WriteLine("Умножение числа на полином равно:");
+                            var result = polynom * multiplier;
+                            result.PrintPol();
+                            break;
+                        }
+
+                    case 5:
+                        {
+                            Console.WriteLine("Введите первый полином:");
+                            var polynom1 = InputPolynom();
+                            Console.WriteLine("Введите второй полином:");
+                            var polynom2 = InputPolynom();
+                            Console.WriteLine("Произведение введенных полиномов равно:");
+                            var result = polynom1 * polynom2;
+                            result.PrintPol();
+                            break;
+                        }
+
+                    case 6:
+                        {
+                            Console.WriteLine("Введите начало и конец отрезка на котором находится корень");
+                            var start = Convert.ToDouble(Console.ReadLine());
+                            var end = Convert.ToDouble(Console.ReadLine());
+                            var variable = -1;
+                            while (variable != 0)
+                            {
+                                Console.WriteLine("Выберите метод решения:");
+                                Console.WriteLine("1. Бинарный поиск");
+                                Console.WriteLine("2. Метод Ньютона");
+                                Console.WriteLine("0. Exit");
+                                variable = Convert.ToInt16(Console.ReadLine());
+                                Console.WriteLine("Введите полином");
+                                IFindroot polynom;
+                                switch (variable)
+                                {
+                                    case 1:
+                                        {
+                                            polynom = InputPolynom();
+                                            var result = polynom.FindRoot(start, end);
+                                            Console.WriteLine("Root is " + result);
+                                            break;
+                                        }
+
+                                    case 2:
+                                        {
+                                            polynom = new Polynom2();
+                                            var result = polynom.FindRoot(start, end);
+                                            Console.WriteLine("Root is " + result);
+                                            break;
+                                        }
+                                }
+                            }
+
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
---
+++ Lab_Mnozhestva/Polinom_Library/Program.cs
@@ -0,0 +1,223 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   Defines the IFindroot type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Polinom_Library
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+
+    public interface IFindroot
+    {
+        double FindRoot(double start, double end);
+    }
+
+    public class Polynom2 : IFindroot
+    {
+        public double FindRoot(double start, double end)
+        {
+            ////Метод Ньютона
+            return -1;
+        }
+    }
+
+    [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Reviewed. Suppression is OK here.")]
+    public class Polynom : IFindroot
+    {
+        private readonly int degree;
+        private readonly List<double> polinom = new List<double>();
+       
+        public Polynom(int degree, IList<double> array)
+        {
+            this.degree = degree;
+            for (var i = 0; i < degree + 1; i++)
+            {
+                this.polinom.Add(array[i]);
+            }
+        }
+
+        [SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1202:ElementsMustBeOrderedByAccess",
+            Justification = "Reviewed. Suppression is OK here.")]
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1303:ConstFieldNamesMustBeginWithUpperCaseLetter", Justification = "Reviewed. Suppression is OK here.")]
+        public double FindRoot(double start, double end)
+        {
+            const double Eps = 0.000001;
+            var fstart = this.Calc(start);
+            while (Math.Abs(start - end) > Eps)
+            {
+                var center = (start + end) / 2;
+                var fcenter = this.Calc(center);
+                if (fstart * fcenter <= 0)
+                {
+                    end = center;
+                }
+                else
+                {
+                    start = center;
+                    fstart = fcenter;
+                }
+            }
+
+            return start;
+        }
+
+        public void PrintPol()
+        {
+            for (var i = this.degree; i > 0; i--)
+            {
+                var sign = '+';
+                var per = this.polinom[i];
+                if (per < 0)
+                {
+                    per = -per;
+                    sign = (char)43;
+                }
+
+                Console.Write(per + "*x^" + i + " " + sign + " ");
+            }
+
+            Console.WriteLine(this.polinom[0]);
+        }
+        
+        public double[] Calculate(double[] array)
+        {
+            var values = new double[array.Length];
+            for (var i = 0; i < array.Length; i++)
+            {
+                values[i] = 0;
+                values[i] = this.Calc(array[i]);
+            }
+
+            return values;
+        }
+
+        private double Calc(double value)
+        {
+            var results = 0.0;
+            var j = 0;
+            foreach (var koef in this.polinom)
+            {
+                results += koef * Math.Pow(value, j);
+                j++;
+            }
+
+            return results;
+        }
+
+        [SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1204:StaticElementsMustAppearBeforeInstanceElements", Justification = "Reviewed. Suppression is OK here."),SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1204:StaticElementsMustAppearBeforeInstanceElements", Justification = "Reviewed. Suppression is OK here."),SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1204:StaticElementsMustAppearBeforeInstanceElements", Justification = "Reviewed. Suppression is OK here."),SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1204:StaticElementsMustAppearBeforeInstanceElements", Justification = "Reviewed. Suppression is OK here.")]
+        public static Polynom operator +(Polynom first, Polynom second)
+        {
+            var maxdegree = first.degree >= second.degree ? first.degree : second.degree;
+            
+            var sum = new double[maxdegree + 1];
+            for (var i = 0; i <= maxdegree; i++)
+            {
+                if (i > first.degree)
+                {
+                    sum[i] = second.polinom[i];
+                }
+                else if (i > second.degree)
+                {
+                    sum[i] = first.polinom[i];
+                }
+                else
+                {
+                    sum[i] = first.polinom[i] + second.polinom[i];
+                }
+            }
+
+            var summ = new Polynom(maxdegree, sum);
+            return summ;
+        }
+
+        public static Polynom operator -(Polynom first, Polynom second)
+        {
+            if (first == null)
+            {
+                throw new ArgumentNullException("first");
+            }
+
+            int maxdegree;
+            if (first.degree > second.degree)
+            {
+                maxdegree = first.degree;
+            }
+            else if (first.degree < second.degree)
+            {
+                maxdegree = second.degree;
+            }
+            else
+            {
+                maxdegree = first.degree;
+                while (Math.Abs(first.polinom[maxdegree] - second.polinom[maxdegree]) < 0.00001)
+                {
+                    maxdegree--;
+                    if (maxdegree >= 0)
+                    {
+                        continue;
+                    }
+
+                    maxdegree = 0;
+                    break;
+                }
+            }
+
+            var diff = new double[maxdegree + 1];
+            for (var i = 0; i <= maxdegree; i++)
+            {
+                if (i > first.degree)
+                {
+                    diff[i] = -second.polinom[i];
+                }
+                else if (i > second.degree)
+                {
+                    diff[i] = first.polinom[i];
+                }
+                else
+                {
+                    diff[i] = first.polinom[i] - second.polinom[i];
+                }
+            }
+
+            var difference = new Polynom(maxdegree, diff);
+            return difference;
+        }
+
+        public static Polynom operator *(Polynom first, double multiplier)
+        {
+            var diff = new double[first.degree + 1];
+            for (var i = 0; i < first.degree + 1; i++)
+            {
+                diff[i] = multiplier * first.polinom[i];
+            }
+
+            var difference = new Polynom(first.degree, diff);
+            return difference;
+        }
+
+
+
+        public static Polynom operator *(Polynom first, Polynom second)
+        {
+            var maxdegree = first.degree + second.degree;
+            var compostion = new double[maxdegree + 1];
+
+            for (var i = 0; i < first.degree + 1; i++)
+            {
+                for (var j = 0; j < second.degree + 1; j++)
+                {
+                    compostion[j + i] += first.polinom[i] * second.polinom[j];
+                }
+            }
+
+            var result1 = new Polynom(maxdegree, compostion);
+            return result1;
+        }
+    }
+}
